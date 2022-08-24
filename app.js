1c1170f329bc06d3cbd4c22ea6f18934613828b8
pvalle0392 <pvalle0392@gmail.com>
2022-08-23 22:44:45 -0500

    08-23-2022 commit

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ var app = express();
 
 //Conexion a la base  de datos
 connectDB();
-
+//
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 //middlewares
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
